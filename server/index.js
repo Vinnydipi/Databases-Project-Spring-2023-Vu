4d8100a1939c4b2ecfae36fd79cd4e2981138e25
vincent desk <vincentdp02@gmail.com>
2023-03-08 11:17:42 -0500

    made stuff work

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const cors = require('cors');
 
 const app = express();
@@ -14,14 +14,16 @@ const db = mysql.createConnection({
     database: 'projectvu',
 });
 
+// Used to put user information into the database
 app.post('/register', (req, res) => {
 
     const username = req.body.username;
     const password = req.body.password;
     const email = req.body.email;
+    const userType = req.body.userType;
 
-    db.query("INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
-     [username, email, password], 
+    db.query("INSERT INTO users (username, email, password, userType) VALUES (?, ?, ?, ?)",
+     [username, email, password, userType], 
      (err, result) => {
         if (err)
         {
@@ -30,11 +32,37 @@ app.post('/register', (req, res) => {
         }
         else
         {
-            console.log('User Registered Succesfully');
+            console.log('User Registered Succesfully, Login To View Events');
             res.status(200).send('User Registered Succeesfully');
         }
      }
     );
+});
+
+// Used to check if the user information entered from the login
+// page is in the database and if so we move to the events page
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    db.query("SELECT * FROM users WHERE username = ? AND password = ?",
+    [username, password], 
+    (err, result) => {
+        if (err)
+        {
+            res.send({err: err});
+        }
+        
+        if (result.length > 0) 
+        {
+            res.send(result);
+        }
+        else
+        {
+            res.send({message: "Wrong username/password"});
+        } 
+    }
+   );
 });
 
 app.listen(3001, () => {
