b1d5fcc853354f902a83b031edc8fe50907112b0
vincent <vincentdp02@gmail.com>
2023-03-23 20:34:52 -0400

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,10 @@ import Axios from 'axios';
 import Register from './pages/register';
 import Login from './pages/login';
 import Events from './pages/events';
+import CreateRso from './pages/components/createRso';
+import ViewRso from './pages/components/viewRso';
+import CreateEvent from './pages/components/createEvent';
+import CreateUni from './pages/components/createUni';
 
 function App() 
 {    
@@ -32,6 +36,10 @@ function App()
           <Route path="/" element={<Login setLoginStatus={setLoginStatus} />} />
           <Route path="/register" element={<Register />} />
           <Route path="/events" element={loginStatus ? <Events /> : <Login setLoginStatus={setLoginStatus} />} />
+          <Route path="/createRso" element={loginStatus ? <CreateRso /> : <Login setLoginStatus={setLoginStatus} /> } />
+          <Route path="/viewRso" element={loginStatus ? <ViewRso /> : <Login setLoginStatus={setLoginStatus} /> } />
+          <Route path="/createEvent" element={loginStatus ? <CreateEvent /> : <Login setLoginStatus={setLoginStatus} /> } />
+          <Route path="/createUni" element={loginStatus ? <CreateUni /> : <Login setLoginStatus={setLoginStatus} /> } />
         </Routes>
       </BrowserRouter>
     </>
--- client/src/pages/events.js
+++ client/src/pages/events.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Axios from 'axios';
 
 // Importing the logout feature
@@ -8,13 +9,17 @@ import { logout } from './utils/logout';
 function Events()
 {
 
+    // Used to navigate around the web app
+    const navigate = useNavigate();
+
     return(
+
         <div className='events'>
         {/* Buttons */}
-        <button className="button" >Add Event</button>
-        <button className="button" >Create RSO</button>
-        <button className="button" >Join RSO</button>
-        <button className="button" >Create University Account</button>
+        <button className="button" onClick={()=>navigate('/createEvent')}>Create Event</button>
+        <button className="button" onClick={()=>navigate('/createRso')}>Create RSO</button>
+        <button className="button" onClick={()=>navigate('/viewRso')}>View RSO's</button>
+        <button className="button" onClick={()=>navigate('/createUni')}>Create University</button>
         {/* Logs the user out*/}
         <button className="button" onClick={ logout }>Logout</button>
         
