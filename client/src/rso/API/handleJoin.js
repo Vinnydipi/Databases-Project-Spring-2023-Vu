3a5886e63e1776fc9c62e6c83ca448fadb85acf5
vincent <vincentdp02@gmail.com>
2023-05-18 15:53:25 -0400

    final version? I think

--- client/src/rso/API/handleJoin.js
+++ client/src/rso/API/handleJoin.js
@@ -2,18 +2,18 @@ import Axios from 'axios';
 
 // Sends the information for joining an RSO to the backend
 export const handleJoinRso = (rsoId, userId) =>
+{
+    Axios.post(`http://localhost:3001/mainPage/MainRso/join?rsoId=${rsoId}&userId=${userId}`)
+    .then((response) => 
     {
-        Axios.post(`http://localhost:3001/mainPage/MainRso/join?rsoId=${rsoId}&userId=${userId}`)
-        .then((response) => 
-        {
-            // Handles success response
-            console.log(response);
-            alert("You have joined an RSO!");
-        })
-        .catch((error) =>
-        {
-            // Handle error response
-            console.log(error);
-            alert("Error Joining RSO, try again!");
-        })
-    };
+        // Handles success response
+        console.log(response);
+        alert("You have joined an RSO!");
+    })
+    .catch((error) =>
+    {
+        // Handle error response
+        console.log(error);
+        alert("Error Joining RSO, try again!");
+    })
+};
--- client/src/rso/rsoPage.jsx
+++ client/src/rso/rsoPage.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import './rsoStyle.css';
+
 function RsoPage({ rsoList, rsoName, setRsoName, navigate, handleJoinRso, handleLeaveRso, handleSubmitNewRso, logout, uniId, userId })
 {
     return (
@@ -8,59 +10,64 @@ function RsoPage({ rsoList, rsoName, setRsoName, navigate, handleJoinRso, handle
                 <button onClick={ logout }>Logout</button>
                 <button onClick={() => navigate('/mainPage')}>Home</button>
             </div>
-            <div className='inputForm'>
-                RSO PAGE
-                <form className='createRsoForm' id='createRsoForm' onSubmit={(e) =>
-                {
-                    e.preventDefault();
-                    handleSubmitNewRso(rsoName, uniId, userId);
-                }}>
-                    <h2>Submit New RSO</h2>
-                    <label htmlFor='name'>RSO Name:</label>
-                    <input type='text' id='name' onChange={(e) =>
-                    {
-                        setRsoName(e.target.value);
-                    }}></input>
-                    <br></br>
-                    <button type='submit'>Submit </button>
-                </form>
-            </div>
-            <div className='table'>
-                <h2>Active Rso's</h2>
-                <table>
-                    <thead>
-                            <tr>
-                                <th>RSO Name</th>
-                                <th>University</th>
-                                <th>Total Members</th>
-                                <th>Status</th>
-                                <th>Member</th>
-                                <th>Actions</th>
-                            </tr>
-                    </thead>
-                    <tbody>
-                        {rsoList.map((rso) => (
-                            <tr key={rso.rsoId}>
-                                <td>{rso.name}</td>
-                                <td>{rso.universityId === 1 ? 'UCF' : 'N/A'}</td>
-                                <td>{rso.memberCount}</td>
-                                <td>{rso.status}</td>
-                                <td>{rso.isMember ? 'Member' : 'Not a Member'}</td>
-                                <td>
-                                    <button onClick={() => 
-                                    {
-                                        if (rso.isMember)
-                                            handleLeaveRso(rso.rsoId, userId);
-                                        else
-                                            handleJoinRso(rso.rsoId, userId);
-                                    }}>
-                                        {rso.isMember ? 'Leave' : 'Join'}
-                                    </button>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+            <div className='rso-container'>
+                <div className='rso-inputForm-container'>
+                    <div className='rso-inputForm'>
+                        <form className='createRsoForm' id='createRsoForm' onSubmit={(e) =>
+                        {
+                            e.preventDefault();
+                            handleSubmitNewRso(rsoName, uniId, userId);
+                        }}>
+                            <h2>Submit New RSO</h2>
+                            <label htmlFor='name'>RSO Name:</label>
+                            <input placeholder='Must Be Unique' type='text' id='name' onChange={(e) =>
+                            {
+                                setRsoName(e.target.value);
+                            }}></input>
+                            <br></br>
+                            <button type='submit'>Submit</button>
+                        </form>
+                    </div>
+                </div>   
+                <div className='rso-table-container'>     
+                    <div className='table'>
+                        <h2>Joinable Rso's</h2>
+                        <table>
+                            <thead>
+                                    <tr>
+                                        <th>RSO Name</th>
+                                        <th>University</th>
+                                        <th>Total Members</th>
+                                        <th>Status</th>
+                                        <th>Member</th>
+                                        <th>Actions</th>
+                                    </tr>
+                            </thead>
+                            <tbody>
+                                {rsoList.map((rso) => (
+                                    <tr key={rso.rsoId}>
+                                        <td>{rso.name}</td>
+                                        <td>{rso.universityId === 1 ? 'UCF' : 'N/A'}</td>
+                                        <td>{rso.memberCount}</td>
+                                        <td>{rso.status}</td>
+                                        <td>{rso.isMember ? 'Member' : 'Not a Member'}</td>
+                                        <td>
+                                            <button onClick={() => 
+                                            {
+                                                if (rso.isMember)
+                                                    handleLeaveRso(rso.rsoId, userId);
+                                                else
+                                                    handleJoinRso(rso.rsoId, userId);
+                                            }}>
+                                                {rso.isMember ? 'Leave' : 'Join'}
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>                              
             </div>
         </>
     )
